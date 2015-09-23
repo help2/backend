a0fd96fb04252db3c2a4dde532a39dbc01d86d45
Manuel Meurer <manuel@krautcomputing.com>
2015-09-23 16:03:27 +0200

    remove duplicate TEMPLATES from settings.py

--- heart/settings.py
+++ heart/settings.py
@@ -183,30 +183,3 @@ try:
     from settings_local import *
 except ImportError:
     pass
-
-from django.conf import global_settings
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            # insert your TEMPLATE_DIRS here
-        ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                # list if you haven't customized them:
-                'django.contrib.auth.context_processors.auth',
-                'django.template.context_processors.debug',
-                'django.template.context_processors.i18n',
-                'django.template.context_processors.media',
-                'django.template.context_processors.static',
-                'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.request'
-            ]
-        },
-    },
-]
-
-BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
